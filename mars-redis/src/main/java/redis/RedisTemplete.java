c34e9f8295ed1dfea16ac1cab12e2941c177f03b
yuye <631941527@qq.com>
2019-10-05 21:38:01 +0800

    update to 3.0.0

---
+++ mars-redis/src/main/java/redis/RedisTemplete.java
@@ -0,0 +1,314 @@
+package redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.ShardedJedis;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Redis操作
+ */
+public class RedisTemplete {
+
+    private static Logger logger = LoggerFactory.getLogger(RedisTemplete.class);// 日志
+
+    /**
+     * 从连接池获取redis连接
+     * @return
+     */
+    public static ShardedJedis getShardedJedis() {
+        ShardedJedis jedis = null;
+        try {
+            jedis = JedisPoolFactory.getShardedJedisPool().getResource();
+        } catch (Exception e) {
+            logger.error("get Jedis error", e);
+        }
+
+        return jedis;
+    }
+
+    /**
+     * 模糊查询keys
+     *
+     * @param pattern
+     * @return
+     */
+    public static TreeSet<String> keys(String pattern) {
+        ShardedJedis jedis = getShardedJedis();
+        TreeSet<String> allKeys = new TreeSet<>();
+
+        try {
+            Collection<Jedis> jedisList = jedis.getAllShards();// 获取所有的缓存实例
+            for (Jedis jedis2 : jedisList) {
+                allKeys.addAll(jedis2.keys(pattern));// 获取匹配prefix的所有的key
+            }
+        } catch (Exception e) {
+            logger.error("Getting keys error: {}", e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return allKeys;
+    }
+
+    /**
+     * 设置redis值
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean set(String key, String value) {
+        return set(key.getBytes(),value.getBytes());
+    }
+
+    /**
+     * 设置redis值
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean set(byte[] key, byte[] value) {
+        ShardedJedis jedis = getShardedJedis();
+        try {
+            if (logger.isInfoEnabled()) {
+                logger.info("set redis value:" + key + "@" + value);
+            }
+            jedis.set(key, value);
+            return true;
+        } catch (Exception e) {
+            logger.error("set redis value error:" + key + "@" + value, e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return false;
+    }
+
+    /**
+     * 设置redis值
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean setex(byte[] key, byte[] value,Integer ex) {
+        ShardedJedis jedis = getShardedJedis();
+        try {
+            if (logger.isInfoEnabled()) {
+                logger.info("set redis value:" + key + "@" + value);
+            }
+            jedis.setex(key,ex,value);
+            return true;
+        } catch (Exception e) {
+            logger.error("set redis value error:" + key + "@" + value, e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return false;
+    }
+
+    /**
+     * 设置redis值
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean setex(String key, String value,Integer ex) {
+        return setex(key.getBytes(),value.getBytes(),ex);
+    }
+
+    /**
+     * 获取redis值
+     *
+     * @param key
+     * @return
+     */
+    public static String get(String key) {
+        byte[] result = get(key.getBytes());
+        return new String(result);
+    }
+
+    /**
+     * 获取redis值
+     *
+     * @param key
+     * @return
+     */
+    public static byte[] get(byte[] key) {
+        ShardedJedis jedis = getShardedJedis();
+        try {
+            if (logger.isInfoEnabled()) {
+                logger.info("get redis value:" + key);
+            }
+
+            return jedis.get(key);
+        } catch (Exception e) {
+            logger.error("get redis value error:" + key, e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return null;
+    }
+
+
+    /**
+     * 回收redis连接
+     * @param jedis
+     */
+    public static void recycleJedis(ShardedJedis jedis) {
+        if (jedis != null) {
+            try {
+                jedis.close();
+            } catch (Exception e) {
+                logger.error("recycleJedis error" + jedis, e);
+            }
+        }
+    }
+
+
+    /**
+     * 根据key删除数据
+     * @param key
+     */
+    public static void del(String key) {
+        del(key.getBytes());
+    }
+
+    /**
+     * 根据key删除数据
+     * @param key
+     */
+    public static void del(byte[] key) {
+        ShardedJedis jedis = getShardedJedis();
+        if (jedis != null) {
+            try {
+                jedis.del(key);
+            } catch (Exception e) {
+                logger.error("delString error:" + key, e);
+            } finally {
+                recycleJedis(jedis);
+            }
+        }
+    }
+
+    /**
+     * 更新byte类型的数据，主要更新过期时间
+     * @param key
+     * @param expireTimeSecond
+     */
+    public static void updateObjectEnableTime(byte[] key, int expireTimeSecond) {
+        ShardedJedis jedis = getShardedJedis();
+        if (jedis != null) {
+            try {
+                jedis.expire(key, expireTimeSecond);
+            } catch (Exception e) {
+                logger.error("updateObject error:" + key, e);
+            } finally {
+                recycleJedis(jedis);
+            }
+        }
+
+    }
+
+    /**
+     * 更新String类型的数据，主要更新过期时间
+     * @param key
+     * @param expireTimeSecond
+     */
+    public static void updateObjectEnableTime(String key, int expireTimeSecond) {
+        updateObjectEnableTime(key.getBytes(),expireTimeSecond);
+    }
+
+    /**
+     * 获取所有keys
+     * @param pattern
+     * @return
+     */
+    public static Set<String> getAllKeys(String pattern) {
+        ShardedJedis jedis = getShardedJedis();
+        if (jedis != null) {
+            try {
+                return jedis.hkeys(pattern);
+            } catch (Exception e) {
+                logger.error("getAllKeys error:" + pattern, e);
+            } finally {
+                recycleJedis(jedis);
+            }
+        }
+        return null;
+    }
+
+
+
+    /**
+     * 设置值
+     * @param group
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean hset(String group,String key, String value) {
+        ShardedJedis jedis = getShardedJedis();
+        try {
+
+            jedis.hset(group,key, value);
+            return true;
+        } catch (Exception e) {
+            logger.error("set redis value error:" + key + "@" + value, e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return false;
+    }
+
+    /**
+     * 获取单个值
+     * @param group
+     * @param key
+     * @return
+     */
+    public static String hget(String group, String key){
+        ShardedJedis jedis = getShardedJedis();
+        String result = "";
+        try {
+            result = jedis.hget(group, key);
+        } catch (Exception e) {
+            logger.error("hget redis value error:" + group + "@" + key, e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return result;
+    }
+
+    /**
+     * 获取一组数据
+     * @param group
+     * @return
+     */
+    public static Map<String, String> hgetAll(String group){
+        ShardedJedis jedis = getShardedJedis();
+        Map<String, String>  result = null;
+        try {
+            result = jedis.hgetAll(group);
+        } catch (Exception e) {
+            logger.error("hgetAll redis value error:" + group, e);
+        } finally {
+            recycleJedis(jedis);
+        }
+
+        return result;
+    }
+
+}
