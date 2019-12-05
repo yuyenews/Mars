db6f5a636c0c8f59c5715a60c273255f86a7b373
yuye <631941527@qq.com>
2019-12-05 13:29:14 +0800

    update version

---
+++ mars-mvc/src/main/java/com/mars/mvc/util/MarsCheckParamsUtil.java
@@ -0,0 +1,14 @@
+package com.mars.mvc.util;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class MarsCheckParamsUtil {
+
+    public static JSONObject checkParams(Object obj){
+        JSONObject result = null;
+
+
+
+        return result;
+    }
+}
