4544a1a98b3cdbdc984859a0d9c9af0eb3df52c6
yuye <631941527@qq.com>
2019-12-22 22:04:03 +0800

    update version

--- mars-jdbc/src/main/java/com/mars/jdbc/helper/model/PageParamModel.java
+++ mars-jdbc/src/main/java/com/mars/jdbc/helper/model/PageParamModel.java
@@ -1,5 +1,7 @@
 package com.mars.jdbc.helper.model;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,30 +13,50 @@ public class PageParamModel {
 
     private int pageSize;
 
-    public Map<String, Object> getParam() {
+    /**
+     * 创建一个分页参数对象
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public static PageParamModel getPageParamModel(Integer page,Integer pageSize){
+        PageParamModel pageParamModel = new PageParamModel();
+        pageParamModel.setCurrentPage(page);
+        pageParamModel.setPageSize(pageSize);
+        return pageParamModel;
+    }
+
+    public Map<String,Object> getParam() {
         if(param == null){
-            return new HashMap<>();
+            param = new HashMap<>();
         }
         return param;
     }
 
-    public void setParam(Map<String, Object> param) {
-        this.param = param;
+    public PageParamModel setParam(Object param) {
+        if(param == null){
+            return this;
+        }
+        String strParam = JSON.toJSONString(param);
+        this.param = JSON.parseObject(strParam,Map.class);
+        return this;
     }
 
     public int getCurrentPage() {
         return currentPage;
     }
 
-    public void setCurrentPage(int currentPage) {
+    public PageParamModel setCurrentPage(int currentPage) {
         this.currentPage = currentPage;
+        return this;
     }
 
     public int getPageSize() {
         return pageSize;
     }
 
-    public void setPageSize(int pageSize) {
+    public PageParamModel setPageSize(int pageSize) {
         this.pageSize = pageSize;
+        return this;
     }
 }
