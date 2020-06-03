2c59a3089bb7a70b6a22ba653a1ffe91c48a7069
yuye <631941527@qq.com>
2020-06-03 20:10:49 +0800

    补充core模块的单测

--- mars-common/src/main/java/com/mars/common/util/MarsAddressUtil.java
+++ mars-common/src/main/java/com/mars/common/util/MarsAddressUtil.java
@@ -75,17 +75,18 @@ public class MarsAddressUtil {
         while (iFaces.hasMoreElements()) {
             NetworkInterface iFace = (NetworkInterface) iFaces.nextElement();
 
-            Enumeration inetAddrs= iFace.getInetAddresses();
+            Enumeration enumeration= iFace.getInetAddresses();
             // 在所有的接口下再遍历IP
-            while (inetAddrs.hasMoreElements()) {
-                InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
-                if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
-                    if (inetAddr.isSiteLocalAddress()) {
+            while (enumeration.hasMoreElements()) {
+                InetAddress inetAddress = (InetAddress) enumeration.nextElement();
+                // 排除loopback类型地址
+                if (!inetAddress.isLoopbackAddress()) {
+                    if (inetAddress.isSiteLocalAddress()) {
                         // 如果是site-local地址，就是它了
-                        return inetAddr;
+                        return inetAddress;
                     } else if (candidateAddress == null) {
                         // site-local类型的地址未被发现，先记录候选地址
-                        candidateAddress = inetAddr;
+                        candidateAddress = inetAddress;
                     }
                 }
             }
