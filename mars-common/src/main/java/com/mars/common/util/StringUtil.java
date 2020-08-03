5f75816eefb5ff70df770d2ab9022a799291a868
姬野永远 <liujch1996@gmail.com>
2020-08-04 00:26:36 +0800

    Update StringUtil.java

--- mars-common/src/main/java/com/mars/common/util/StringUtil.java
+++ mars-common/src/main/java/com/mars/common/util/StringUtil.java
@@ -13,10 +13,7 @@ public class StringUtil {
 	 * @return string
 	 */
 	public static String getFirstLowerCase(String str) {
-		String str2 = str.substring(1);
-		String str3 = str.substring(0,1);
-		
-		return str3.toLowerCase()+str2;
+		return str.substring(0,1).toLowerCase()+str.substring(1);
 	}
 	
 	/**
@@ -25,9 +22,6 @@ public class StringUtil {
 	 * @return string
 	 */
 	public static boolean isNull(Object obj) {
-		if (obj == null || obj.toString().trim().equals("")) {
-			return true;
-		}
-		return false;
+		return obj == null || obj.toString().trim().equals("");
 	}
 }
