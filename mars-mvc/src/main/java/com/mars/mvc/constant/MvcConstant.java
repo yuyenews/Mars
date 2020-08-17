ad3261b8317cc66739cf2104bb094ac414254eaa
yuye <631941527@qq.com>
2020-08-17 20:54:04 +0800

    修复bug

--- mars-mvc/src/main/java/com/mars/mvc/constant/MvcConstant.java
+++ mars-mvc/src/main/java/com/mars/mvc/constant/MvcConstant.java
@@ -8,5 +8,5 @@ public class MvcConstant {
     /**
      * 欢迎语
      */
-    public static final String WELCOME = "欢迎使用Martian框架，希望此框架可以给你带来不同的体验，项目官网:<a href=\"http://mars-framework.com\">http://mars-framework.com</a>";
+    public static final String WELCOME = "欢迎使用Martian框架，希望此框架可以给你带来不同的体验，项目官网:http://mars-framework.com";
 }
