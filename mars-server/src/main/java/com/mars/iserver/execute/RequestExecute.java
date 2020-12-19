9c443a6bc0d2e53463216318a86594550fe0ab9e
yuye <631941527@qq.com>
2020-12-19 11:11:29 +0800

    修复异常提示不显示错误信息的问题

--- mars-server/src/main/java/com/mars/iserver/execute/RequestExecute.java
+++ mars-server/src/main/java/com/mars/iserver/execute/RequestExecute.java
@@ -14,8 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 处理请求的线程
