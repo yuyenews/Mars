7963af8be34c424edfebfe868237b9ad3fb72c24
yuye <631941527@qq.com>
2021-01-01 12:42:15 +0800

    优化性能

--- mars-core/src/test/java/com/mars/core/test/aop/traction/TractionTest.java
+++ mars-core/src/test/java/com/mars/core/test/aop/traction/TractionTest.java
@@ -1,23 +1,24 @@
 package com.mars.core.test.aop.traction;
 
 import com.alibaba.fastjson.JSON;
+import com.mars.aop.base.BaseTraction;
 import com.mars.aop.model.AopModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TractionTest {
+public class TractionTest implements BaseTraction {
 
     private Logger logger = LoggerFactory.getLogger(TractionTest.class);
 
-    public void startMethod(AopModel aopModel) {
+    public void beginTraction(AopModel aopModel) {
         logger.info("单测开始执行事务开启方法，事务级别:{}", JSON.toJSONString(aopModel.getTractionLevel()));
     }
 
-    public void endMethod() {
+    public void commit() {
         logger.info("单测开始执行事务提交方法");
     }
 
-    public void exp(Throwable e) {
+    public void rollback(Throwable e) {
         logger.info("事务单测出异常",e);
     }
 }
